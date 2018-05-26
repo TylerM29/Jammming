de9bd2f587ca158c7caeda7ad890398cb459e012
TylerM29 <37600011+TylerM29@users.noreply.github.com>
2018-05-26 15:17:19 -0600

    Update SearchResults.js

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -3,10 +3,20 @@ import TrackList from '../TrackList/TrackList';
 import './SearchResults.css';
 
 class SearchResults extends React.Component {
+  SearchResults() {
+    //this.state.searchResults;
+    this.setState({ searchResults: this.props.searchResults })
+    this.addTrack = this.addTrack.bind(this);
+  }
   render() {
     return (
       <div className="SearchResults">
   <h2>Results</h2>
+  {/this.props is an external call/}
+  <TrackList tracks={this.props.searchResults}
+  onAdd={this.props.onAdd}
+  isRemoval={false}
+  onAdd={SearchResults}/>
 </div>
     );
   }
