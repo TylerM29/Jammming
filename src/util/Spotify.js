ffc9fec1dc812d18dc282162daf4885f0ea69bf3
TylerM29 <37600011+TylerM29@users.noreply.github.com>
2018-05-29 22:26:49 -0600

    Add files via upload

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,22 +1,21 @@
 let accessToken;
 let expiresIn;
 const clientID = 'bf8d14583fcf4789bf7c5f4a36f17cd5';
-const redirectURI = "https://tylers-project.surge.sh";
+const redirectURI = "http://localhost:3000";
 
 const Spotify = {
   getAccessToken() {
-    if(accessToken) {
-      return accessToken;
-    }
     const url = window.location.href;
     const token = url.match(/access_token=([^&]*)/);
     const expires = url.match(/expires_in=([^&]*)/);
-    if(token && expires) {
+    if(accessToken) {
+      return accessToken;
+    } else if (token && expires) {
       accessToken = token[1];
       expiresIn = expires[1];
-      window.setTimeout(() => accessToken = '', expiresIn * 1000);
-      window.history.pushState('Access Token', null, '/');
-      return accessToken;
+        window.setTimeout(() => accessToken = null, expiresIn * 1000);
+        window.history.pushState('Access Token', null, '/');
+          return accessToken;
     } else {
       window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
     }
@@ -25,10 +24,10 @@ const Spotify = {
       this.getAccessToken();
       return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
         {
-          headers: `Bearer ${accessToken}`,
+          Authorization: `Bearer ${accessToken}`,
           'Content-type': 'application/json'
         }
-      ,).then(response =>{
+      ).then(response =>{
         return response.json();
       }).then(jsonResponse =>{
         if(!jsonResponse.tracks){return [];}
